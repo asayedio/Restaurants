ed95fe2628c820a06910dbe3530253f2fabbe1e8
asayed <ahmedsayed.9191@gmail.com>
2020-09-24 22:23:13 +0200

    Finaize all the required packages

---
+++ Restaurant.Data/RestaurantsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Data
+{
+    public class RestaurantsDbContext : DbContext
+    {
+        public DbSet<RestaurantClass> Restaurants { get; set; }
+    }
+}
