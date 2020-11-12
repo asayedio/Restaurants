c5fcb9d87bfc80ebcfe540f8d3244eea7109be4e
asayedio <ahmedsayed.9191@gmail.com>
2020-11-12 23:59:33 +0200

    Automate database migrations

--- Restaurants/Program.cs
+++ Restaurants/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Restaurant.Data;
 
 namespace Restaurants
 {
@@ -13,7 +16,18 @@ namespace Restaurants
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            MigrateDatabase(host);
+            host.Run();
+        }
+
+        private static void MigrateDatabase(IHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<RestaurantsDbContext>();
+                db.Database.Migrate();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
